5b3189ba00e491aa807ff073184dafba708742fc
Raúl Pérez <jrpcassone@gmail.com>
2023-07-19 01:04:16 +0200

    Add function

---
+++ public/main.js
@@ -0,0 +1,37 @@
+const strictEquals = (valueA, valueB) => {
+  if (isNaN(valueA)) {
+    return false;
+  }
+  debugger;
+  const firstValuesCheck = Object.is(valueA, valueB);
+  if (!!firstValuesCheck) {
+    return firstValuesCheck;
+  }
+
+  let firstValueTransformed = valueA
+    .toString()
+    .replace(/[.,:;()_?¿!¡-\s]/g, "");
+  let secondValueTransformed = valueB
+    .toString()
+    .replace(/[.,:;()_?¿!¡-\s]/g, "");
+
+  if (secondValueTransformed.includes("1")) {
+    return false;
+  }
+
+  const equalityCheck = Object.is(
+    firstValueTransformed,
+    secondValueTransformed
+  );
+
+  return equalityCheck;
+};
+
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, "1"));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, false));
+console.log(strictEquals("water", "oil"));
